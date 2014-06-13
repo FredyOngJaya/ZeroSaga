8e666f5ac75a01aebec4d952ee9effdc2a06f3ab
Erwin <rw7nz.cyruz@gmail.com>
2014-06-13 16:23:18 +0700

    use functional object creation. make player still in running state when moving to the edge.

--- scripts/world.js
+++ scripts/world.js
@@ -1,85 +1,109 @@
-var zero = {
-    el: document.getElementById("zero"),
-    transformed: false,
-    running: false,
-    getPosX: function () {
-        var style = window.getComputedStyle(this.el),
-            left  = style.left,
-            posX  = parseInt(left.substr(0, left.length - 2), 10);
-        
-        return posX;
+var Stage = function (id) {
+  var _elem = document.getElementById(id),
+      _style = window.getComputedStyle(_elem),
+      _width = parseInt(_style.width, 10),
+      _height = parseInt(_style.height, 10);
+  
+  return {
+    getWidth: function () {
+      return _width; 
     },
-    standStill: function () {
-        if (this.running) {
-            this.el.setAttribute('class', 'standstill');
-            this.running = false;
-        }
-    },
-    moveRight: function () {
-        var posX   = this.getPosX(),
-            newPos = posX + 5;
-        
-        if (posX > 945) {
-            return;
-        }
-        
-        if (!this.running) {
-            this.el.classList.remove('standstill');
-			this.el.classList.add('start-running');
-            this.running = true;
-        } else {
-            this.el.classList.remove('start-running');
-			this.el.classList.add('running');
-		}
-        
-        if (this.transformed) {
-            this.el.classList.remove('go-left');
-            this.transformed = !this.transformed;
-        }
-        
-        this.el.style.left = newPos + "px";
-    },
-    moveLeft: function () {
-        var posX   = this.getPosX(),
-            newPos = posX - 5;
-        
-        if (posX < 0) {
-            return;
-        }
-        
-        if (!this.running) {
-            this.el.classList.remove('standstill');
-            this.el.classList.add('start-running');
-            this.running = true;
-        } else {
-            this.el.classList.remove('start-running');
-            this.el.classList.add('running');
-		}
-        
-        if (!this.transformed) {
-            this.el.classList.add('go-left');
-            this.transformed = !this.transformed;
-        }
-        
-        this.el.style.left = newPos + "px";
+    getHeight: function () {
+      return _height;
     }
+  };
 };
 
-window.addEventListener("keydown", function (e) {
-    var code = e.keyCode;
-    
-    switch (code) {
-    case 37:
-    case 65:
-        zero.moveLeft();
-        break;
-    case 39:
-    case 68:
-        zero.moveRight();
-        break;
-    }
+var Player = function (id) {
+  var _elem = document.getElementById(id),
+      _style = window.getComputedStyle(_elem),
+      _class = _elem.classList,
+      _width = parseInt(_style.width, 10),
+      _height = parseInt(_style.height, 10),
+      _posX = parseInt(_style.left, 10),
+      _posY = _height - parseInt(_style.top, 10),
+      _speed = 5,
+      _transformed = false,
+      _running = false;
+
+  var _standStill = function () {
+      if (_running) {
+        _class.remove('start-running');
+        _class.remove('running');
+        _class.add('standstill');
+        _running = false;
+      }
+    },
+    _moveRight = function () {
+      if (!_running) {
+        _class.remove('standstill');
+        _class.add('start-running');
+        _running = true;
+      } else {
+        _class.remove('start-running');
+        _class.add('running');
+      }
+
+      if (_transformed) {
+        _class.remove('go-left');
+        _transformed = !_transformed;
+      }
+      
+      if (_posX >= map.getWidth() - _width) {
+        return;
+      }
+
+      _posX += _speed;
+      _elem.style.left = _posX + 'px';
+    },
+    _moveLeft = function () {
+      if (!_running) {
+        _class.remove('standstill');
+        _class.add('start-running');
+        _running = true;
+      } else {
+        _class.remove('start-running');
+        _class.add('running');
+      }
+
+      if (!_transformed) {
+        _class.add('go-left');
+        _transformed = !_transformed;
+      }
+      
+      if (_posX <= 0) {
+        return;
+      }
+
+      _posX -= _speed;
+      _elem.style.left = _posX + 'px';
+    };
+
+  return {
+    standStill: _standStill,
+    moveRight: _moveRight,
+    moveLeft: _moveLeft
+  };
+};
+
+var map = Stage('viewport');
+var zero = Player('zero');
+
+window.addEventListener('keydown', function (e) {
+  var code = e.keyCode;
+
+  switch (code) {
+  case 37:
+  case 65:
+    zero.moveLeft();
+    break;
+  case 39:
+  case 68:
+    zero.moveRight();
+    break;
+  }
 });
 
-window.addEventListener("keyup", function () {
-    zero.standStill();
+window.addEventListener('keyup', function () {
+  zero.standStill();
 });
